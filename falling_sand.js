eac6ebdd7b9a93fc06fffc93755d956094a6563c
User <User>
2017-05-14 21:21:38 +0100

    add somewhat realistic water

--- falling_sand.js
+++ falling_sand.js
@@ -35,79 +35,186 @@ function getPosition(event) {
 
 function UpdateParticle(y, x) {
     // add sand every so often
-    if (input_tick == 300 && pixel[10][75] == 0){
+    if (input_tick > 30000){
         input_tick = 0;
         pixel[10][75] = 1;
-    }
-    if (input_tick == 300 && pixel[10][75] != 0){
-        input_tick = 0;
+        pixel[10][225] = 3;
     }
     input_tick++;
     //is static particle
-    if (pixel[y][x] != 1 && pixel[y][x] != 1)
+    if (pixel[y][x] != 1 && pixel[y][x] != 3)
         return false;
-    // nothing under the particle, keep falling 
-    var under = pixel[y+1][x];
-    if (under == 0)
-    {
-        var isPaused = 0;
-        for (var i = 0; i < paused.length; i++) {
-            if (x == paused[i].x && y == paused[i].y){
-                paused.splice(i,1);
-                isPaused = 1;
+    
+    if (pixel[y][x] == 1){
+        // sand falling behavior
+        // nothing under the particle, keep falling 
+        var under = pixel[y+1][x];
+        if (under == 0)
+        {
+            var isPaused = 0;
+            for (var i = 0; i < paused.length; i++) {
+                if (x == paused[i].x && y == paused[i].y){
+                    paused.splice(i,1);
+                    isPaused = 1;
+                }
+            } 
+            if (!isPaused){
+                pixel[y+1][x] = pixel[y][x];
+                pixel[y][x] = 0;
+                paused.push({x: x, y: y});
+                return true;
             }
-        } 
-        if (!isPaused){
-            pixel[y+1][x] = 1;//pixel[y][x];
-            pixel[y][x] = 0;
-            paused.push({x: x, y: y});
-            return true;
+            if (isPaused){
+                //return true;
+            }
         }
-    }
-    // test left and right supports under the particle 
+        // test left and right supports under the particle 
 
-    var left  = pixel[y+1][x+1] != 0 ? true : false;
-    var right = pixel[y+1][x-1] != 0 ? true : false;
-  
-    // Hit a stable ground (left/right/underneath supports), return that the particle is finished
-    if (left & right)
-        return false;
+        var left  = pixel[y+1][x+1];// != 0 ? true : false;
+        var right = pixel[y+1][x-1];// != 0 ? true : false;
+      
+        // Hit a stable ground (left/right/underneath supports), return that the particle is finished
+        if (left == 1 && right == 1)// && pixel[y][x] == 1)
+            return false;
 
-   // the right support is missing, tumble the particle towards it 
+       // the right support is missing, tumble the particle towards it 
 
-    if (left && !right)
-    {
-        pixel[y+1][x-1] = pixel[y][x];
-        pixel[y][x] = 0;
-    }
-
-    // the left support is missing, tumble the particle towards it 
-
-    if (!left && right)
-    {
-        pixel[y+1][x+1] = pixel[y][x];
-        pixel[y][x] = 0;
-    }
-
-    // both supports missing, tumble the particle towards either side randomly 
-
-    if (!left && !right)
-    {
-        if (Math.random() > 0.5)
+        if (left == 1 && right == 0 || left == 1 && right == 3)
         {
+            var tempPixel = pixel[y+1][x-1];
             pixel[y+1][x-1] = pixel[y][x];
-            pixel[y][x] = 0;
-        } 
-        else
-        {
-            pixel[y+1][x+1] = pixel[y][x];
-            pixel[y][x] = 0;
+            pixel[y][x] = tempPixel;
         }
+
+        // the left support is missing, tumble the particle towards it 
+
+        if (left == 0 && right == 1 || left == 3 && right == 1)
+        {
+            var tempPixel = pixel[y+1][x+1];
+            pixel[y+1][x+1] = pixel[y][x];
+            pixel[y][x] = tempPixel;
+        }
+
+        // both supports missing, tumble the particle towards either side randomly 
+
+        if (left == 0 && right == 0 || left == 3 && right == 3)
+        {
+            if (Math.random() > 0.5)
+            {
+                var tempPixel = pixel[y+1][x-1];
+                pixel[y+1][x-1] = pixel[y][x];
+                pixel[y][x] = tempPixel;
+            } 
+            else
+            {
+                var tempPixel = pixel[y+1][x+1];
+                pixel[y+1][x+1] = pixel[y][x];
+                pixel[y][x] = tempPixel;
+            }
+        }
+
+        // particle still active
+
+        return true;
     }
 
-    // particle still active
+    if (pixel[y][x] == 3){
+        // water falling behavior
+        // nothing under the particle, keep falling 
+        var under = pixel[y+1][x];
+        if (under == 0)
+        {
+            var isPaused = 0;
+            for (var i = 0; i < paused.length; i++) {
+                if (x == paused[i].x && y == paused[i].y){
+                    paused.splice(i,1);
+                    isPaused = 1;
+                }
+            } 
+            if (!isPaused){
+                pixel[y+1][x] = pixel[y][x];
+                pixel[y][x] = 0;
+                paused.push({x: x, y: y});
+                return true;
+            }
+            if (isPaused){
+                //return true;
+            }
+        }
+        // test left and right supports under the particle 
 
-    return true;
+        var left  = pixel[y+1][x+1];// != 0 ? true : false;
+        var right = pixel[y+1][x-1];// != 0 ? true : false;
+        var straightleft = pixel[y][x+1];
+        var straightright = pixel[y][x-1]
+        // Hit a stable ground (left/right/underneath supports), return that the particle is finished
+        if (left == 1 && right == 1)// && pixel[y][x] == 1)
+            return false;
+
+        if (straightleft == 0 && straightright != 0 && under == 3){
+            var tempPixel = pixel[y][x+1];
+            pixel[y][x+1] = pixel[y][x];
+            pixel[y][x] = tempPixel;
+        }
+        if (straightleft != 0 && straightright == 0 && under == 3){
+            var tempPixel = pixel[y][x-1];
+            pixel[y][x-1] = pixel[y][x];
+            pixel[y][x] = tempPixel;
+        }
+        if (straightleft == 0 && straightright == 0 && under == 3){
+            if (Math.random >= 0.5){
+                var tempPixel = pixel[y][x-1];
+                pixel[y][x-1] = pixel[y][x];
+                pixel[y][x] = tempPixel;
+            } else {
+                var tempPixel = pixel[y][x+1];
+                pixel[y][x+1] = pixel[y][x];
+                pixel[y][x] = tempPixel;        
+            }
+        }
+        
+
+       // the right support is missing, tumble the particle towards it 
+
+        if (left == 1 && right == 0 || left == 3 && right == 0)
+        {
+            var tempPixel = pixel[y+1][x-1];
+            pixel[y+1][x-1] = pixel[y][x];
+            pixel[y][x] = tempPixel;
+        }
+
+        // the left support is missing, tumble the particle towards it 
+
+        if (left == 0 && right == 1 || left == 0 && right == 3)
+        {
+            var tempPixel = pixel[y+1][x+1];
+            pixel[y+1][x+1] = pixel[y][x];
+            pixel[y][x] = tempPixel;
+        }
+
+        // both supports missing, tumble the particle towards either side randomly 
+
+        if (left == 0 && right == 0)
+        {
+            if (Math.random() > 0.5)
+            {
+                var tempPixel = pixel[y+1][x-1];
+                pixel[y+1][x-1] = pixel[y][x];
+                pixel[y][x] = tempPixel;
+            } 
+            else
+            {
+                var tempPixel = pixel[y+1][x+1];
+                pixel[y+1][x+1] = pixel[y][x];
+                pixel[y][x] = tempPixel;
+            }
+        }
+
+        // particle still active
+
+        return true;
+    }
+
 }
 
 function draw(){
@@ -126,28 +233,34 @@ function draw(){
                 ctx.fillRect(x, y, x+1, y+1);
             }*/
             if ( pixel[y][x] == 1 ) {
-
+                // sand
                 ctx.fillStyle = 'orange';
                 ctx.fillRect(x, y, 2, 2);
             }
             if ( pixel[y][x] == 2 ) {
-
+                // wall
                 ctx.fillStyle = 'grey';
                 ctx.fillRect(x, y, 2, 2);
             }
             if ( pixel[y][x] == 3 ) {
-
-                ctx.fillStyle = 'red';
+                // water
+                ctx.fillStyle = 'blue';
+                ctx.fillRect(x, y, 2, 2);
+            }
+            if ( pixel[y][x] == 4 ) {
+                // oil
+                ctx.fillStyle = 'brown';
                 ctx.fillRect(x, y, 2, 2);
             }
             UpdateParticle(y,x);
         }
     }
-
+    if (count == 5){count = 0; paused = [];}
+    count++;
     window.requestAnimationFrame(draw);
 
 }
-
+    count = 0;
     gameWidth = 300;
     gameHeight = 300;
     var canvas = document.getElementById("game");
